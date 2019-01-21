6506a2873b67d0ecb91799445ceb249645f2de0e
hensonchen <hensonchen@tencent.com>
2019-01-21 16:02:40 +0800

    feat: fix index.d.ts & add koa-router middleware type

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,8 @@
 import { Router, Context } from 'egg';
+import { IMiddleware } from 'koa-router'
 
 interface RouterPlus extends Router {
-  namespace(prefix: string, ...middlewares: ((ctx: Context, next: () => Promise<any>) => any)[]): Router;
+  namespace(prefix: string, ...middlewares: (((ctx: Context, next: () => Promise<any>) => any) | IMiddleware)[]): Router;
 }
  
 declare module 'egg' {
