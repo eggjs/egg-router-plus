31c1ea5c830601882d9b220d3473f2d5504f4112
hensonchen <hensonchen@tencent.com>
2019-01-22 10:51:21 +0800

    fix: merge egg Context into IMiddleware

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ import { Router, Context } from 'egg';
 import { IMiddleware } from 'koa-router'
 
 interface RouterPlus extends Router {
-  namespace(prefix: string, ...middlewares: (((ctx: Context, next: () => Promise<any>) => any) | IMiddleware)[]): Router;
+  namespace(prefix: string, ...middlewares: IMiddleware<any, Context>[]): Router;
 }
  
 declare module 'egg' {
